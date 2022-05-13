833d1e0cb2d95d4e400b7dc34d08716c9a384766
IonZav <78862168+IonZav@users.noreply.github.com>
2022-05-13 11:25:02 +0300

    ToDoList
    ToDoList

---
+++ ToDoList/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using ToDoList;
+
+[assembly: OwinStartupAttribute(typeof(Startup))]
+
+namespace ToDoList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
